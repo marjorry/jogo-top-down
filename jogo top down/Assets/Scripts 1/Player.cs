a0b58c6933fc4768dd6580440423b27feaef7209
marjorry <marjorie.s@escolar.ifrn.edu.br>
2025-11-04 16:41:54 -0300

    animation - ca

--- jogo top down/Assets/Scripts 1/Player.cs
+++ jogo top down/Assets/Scripts 1/Player.cs
@@ -17,24 +17,45 @@ public class Player : Personagem1
    
     void Update()
     {
+        andando = false;
+
+        if (arma.rotation.eulerAngles.z > -90 && arma.rotation.eulerAngles.z < 90)
+        {
+            spriteRenderer.flipX = false;
+        }
+        
+        if (arma.rotation.eulerAngles.z > 90 && arma.rotation.eulerAngles.z < 270)
+        {
+            spriteRenderer.flipX = true;
+        }
+        
         if(Input.GetKey(KeyCode.W))
         {
-          gameObject.transform.position += new Vector3(0, getVelocidade() * Time.deltaTime, 0);  
+          gameObject.transform.position += new Vector3(0, getVelocidade() * Time.deltaTime, 0); 
+          andando = true;
+          
         }
         
         if(Input.GetKey(KeyCode.S))
         {
-            gameObject.transform.position -= new Vector3(0, getVelocidade() * Time.deltaTime, 0);  
+            gameObject.transform.position -= new Vector3(0, getVelocidade() * Time.deltaTime, 0); 
+            andando = true;
         }
         
         if(Input.GetKey(KeyCode.D))
         {
             gameObject.transform.position += new Vector3(getVelocidade() * Time.deltaTime, 0, 0);
+            andando = true;
         }
         
         if(Input.GetKey(KeyCode.A))
         {
             gameObject.transform.position -= new Vector3(getVelocidade() * Time.deltaTime, 0, 0);
+            andando = true;
         }
+        
+        animator.SetBool("Andando", andando);
+        
+        
     }
 }
