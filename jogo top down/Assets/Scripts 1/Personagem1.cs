6cc68b022a941326be32f4ef3bcda7dba402838c
marjorry <marjorie.s@escolar.ifrn.edu.br>
2025-11-10 12:06:00 -0300

    10/11

--- jogo top down/Assets/Scripts 1/Personagem1.cs
+++ jogo top down/Assets/Scripts 1/Personagem1.cs
@@ -3,65 +3,15 @@ using UnityEngine;
 public class Personagem1
     : MonoBehaviour
 {
-    [SerializeField]
-    private float velocidade = 5;
-    [SerializeField]
-    private int vida = 10;
-    [SerializeField]
-    private int energia = 100;
-
-    public float getVelocidade()
+  
+    void Start()
     {
-        return this.velocidade;
+        
     }
 
-    public void setVelocidade(float velocidade)
+    
+    void Update()
     {
-        this.velocidade = velocidade;
+        
     }
-    
-    public int getVida()
-    {
-        return this.vida;
-    }
-
-    public void setVida(int vida)
-    {
-        this.velocidade = vida;
-    }  
-    
-    public int getEnergia()
-    {
-        return this.energia;
-    }
-
-    public void setEnergia(int energia)
-    {
-        this.vida = vida;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
--- jogo top down/Assets/Scripts 1/Player.cs
+++ jogo top down/Assets/Scripts 1/Player.cs
@@ -1,61 +1,34 @@
 using UnityEngine;
 
-public class Player : Personagem1
+public class Player : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
-    private Animator animator;
     
-    private bool andando = false;
-
-    public Transform arma;
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
+        
     }
 
    
     void Update()
     {
-        andando = false;
-
-        if (arma.rotation.eulerAngles.z > -90 && arma.rotation.eulerAngles.z < 90)
-        {
-            spriteRenderer.flipX = false;
-        }
-        
-        if (arma.rotation.eulerAngles.z > 90 && arma.rotation.eulerAngles.z < 270)
-        {
-            spriteRenderer.flipX = true;
-        }
-        
         if(Input.GetKey(KeyCode.W))
         {
-          gameObject.transform.position += new Vector3(0, getVelocidade() * Time.deltaTime, 0); 
-          andando = true;
-          
+            
         }
         
         if(Input.GetKey(KeyCode.S))
         {
-            gameObject.transform.position -= new Vector3(0, getVelocidade() * Time.deltaTime, 0); 
-            andando = true;
+            
         }
         
         if(Input.GetKey(KeyCode.D))
         {
-            gameObject.transform.position += new Vector3(getVelocidade() * Time.deltaTime, 0, 0);
-            andando = true;
+            
         }
         
         if(Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.position -= new Vector3(getVelocidade() * Time.deltaTime, 0, 0);
-            andando = true;
+            
         }
-        
-        animator.SetBool("Andando", andando);
-        
-        
     }
 }
