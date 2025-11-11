2100e069d50ceead04242f26efa8d58a065f7064
marjorry <marjorie.s@escolar.ifrn.edu.br>
2025-11-11 11:52:16 -0300

    está andando pra cima

--- jogo top down/Assets/Scripts 1/Player.cs
+++ jogo top down/Assets/Scripts 1/Player.cs
@@ -7,31 +7,64 @@ public class Player : Personagem1
     
     public Transform arma;
 
-    private bool amdando;
+    private bool andando = false;
     
     void Start()
     {
-     
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
+
+    
+
     void Update()
     {
-        
+        andando = false;
+
+        if (arma.rotation.eulerAngles.z > +90 && arma.rotation.eulerAngles.z < 90)
+        {
+            spriteRenderer.flipX = false;
+        }
+            
+        if (arma.rotation.eulerAngles.z > 90 && arma.rotation.eulerAngles.z < 270)
+        {
+            spriteRenderer.flipX = true;
+        } 
+            
         if (Input.GetKey(KeyCode.W))
         {
-           gameObject.transform.position += new Vector3(getVelocidade() * Time.deltaTime, 0);
+           gameObject.transform.position += new Vector3(0, getVelocidade() * Time.deltaTime, 0);
+           andando = true;
         }
+        
         if (Input.GetKey(KeyCode.S))
         {
-            gameObject.transform.position -= new Vector3(getVelocidade() * Time.deltaTime, 0);
+            gameObject.transform.position -= new Vector3(0, getVelocidade() * Time.deltaTime, 0);
+            andando = true;
         }
+        
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.position += new Vector3(getVelocidade() * Time.deltaTime, 0, 0 );
+            andando = true;
         }
+        
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.position -= new Vector3(getVelocidade() * Time.deltaTime, 0, 0 );
+            andando = true;
         }
+        
+        animator.SetBool("andando", andando);
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 }
